a507b7fa8f5befa0916ba47b0d414ddf800831a6
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-27 17:01:22 +0430

    userActions added

---
+++ src/redux/user/userActions.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+import {
+  FETCH_USERS_FAILURE,
+  FETCH_USERS_REQUEST,
+  FETCH_USERS_SUCCESS,
+} from "./userTypes";
+
+function fetchUsersRequest() {
+  return {
+    type: FETCH_USERS_REQUEST,
+  };
+}
+
+function fetchUsersFailure(error) {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error,
+  };
+}
+
+function fetchUsersSuccess(users) {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users,
+  };
+}
+
+export const fetchUsers = () => {
+  return function (dispatch) {
+    dispatch(fetchUsersRequest());
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const usersId = res.data.map((u) => u.id);
+        dispatch(fetchUsersSuccess(usersId));
+      })
+      .catch((error) => {
+        dispatch(fetchUsersFailure(error));
+      });
+  };
+};
