674dd72df6d87f53d09a7689898783e264e94763
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-27 13:00:12 +0430

    icecream container added to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import { Provider } from "react-redux";
-import CakeContainer from "./components/CakeContainer";
 import store from "./redux/store";
+import CakeContainer from "./components/CakeContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
+import IceCreamContainer from "./components/IceCreamContainer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <CakeContainer />
         <HooksCakeContainer />
+        <IceCreamContainer />
       </div>
     </Provider>
   );
